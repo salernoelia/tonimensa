ada32d25b526030d07b1ea32883fb71e5a8d3b1c
salernoelia <putopet@gmail.com>
2024-05-15 00:24:43 +0200

    fix

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,18 @@
-export { default } from "file:///Users/eliasalerno/GitHub/tonimensa/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export default {
+  "@vite/client": {
+    "prefetch": true,
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "/Users/eliasalerno/GitHub/tonimensa/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "isEntry": true,
+    "file": "/Users/eliasalerno/GitHub/tonimensa/node_modules/nuxt/dist/app/entry.js"
+  }
+}
